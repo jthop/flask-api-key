bae83301935219c728ce452b9b05d7c7b79847ca
jthop <jh@mode14.com>
2022-06-15 23:05:29 -0500

    build

--- flask_api_key/__init__.py
+++ flask_api_key/__init__.py
@@ -9,7 +9,7 @@ from .utils import get_ext_config
 from .utils import current_api_key
 
 
-__version__ = '0.2.1'
+__version__ = '0.2.2'
 __author__ = '@jthop'
 __copyright__ = f'Copyright 2022 {__author__}'
 
