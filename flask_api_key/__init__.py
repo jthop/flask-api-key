4ca3942f39fa4401579f64530e59a6d853d39d64
jthop <2892650+jthop@users.noreply.github.com>
2023-06-18 09:18:30 -1000

    auto-commit for v0.2.9

--- flask_api_key/__init__.py
+++ flask_api_key/__init__.py
@@ -9,7 +9,7 @@ from .utils import get_ext_config
 from .utils import current_api_key
 
 
-__version__ = '0.2.8+build.128'
+__version__ = '0.2.9+build.129'
 __author__ = '@jthop'
 __copyright__ = f'Copyright 2022 {__author__}'
 
